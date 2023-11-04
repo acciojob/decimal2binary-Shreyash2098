040a005ef3d916405275f4c75d245f4e7acb4d1c
Shreyash2098 <143548443+Shreyash2098@users.noreply.github.com>
2023-11-05 00:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
+function decimalToBinary(decimalNumber) {
+  if (isNaN(decimalNumber) || decimalNumber < 0) {
+    return "Invalid input";
+  }
+
+  return decimalNumber.toString(2);
 }
 
-window.decimalToBinary = decimalToBinary;
+// Example usage:
+const input1 = 7;
+const binary1 = decimalToBinary(input1);
+console.log(binary1); // Output: "111"
+
+const input2 = 10;
+const binary2 = decimalToBinary(input2);
+console.log(binary2); // Output: "1010"
+
+const input3 = 33;
+const binary3 = decimalToBinary(input3);
+console.log(binary3); // Output: "100001"
